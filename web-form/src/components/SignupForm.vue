d103017cf3834724c35e227d2e7f8606a0f720f6
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-04-16 12:07:41 +0300

    SignUp form is almost done

--- web-form/src/components/SignupForm.vue
+++ web-form/src/components/SignupForm.vue
@@ -1,15 +1,32 @@
 <template>
-  <form>
+  <form @submit.prevent="onSubmit">
     <label for="email">Email :</label>
     <input type="email" name="email" id="email" required v-model="email" />
+
     <label for="password">Password :</label>
     <input type="password" name="password" id="password" required />
+
     <label for="role">Role:</label>
     <select name="role" id="role" v-model="role">
       <option value="developer">Developer</option>
       <option value="designer">Designer</option>
       <option value="manager">Manager</option>
     </select>
+
+    <label for="skills">Skills :</label>
+    <input type="text" id="skills" v-model="tempSkill" @keyup="addSkill" />
+    <p class="skills">
+      <span v-for="skill in skills" :key="skill" @click="deleteSkill(skill)">
+        {{ skill }}
+      </span>
+    </p>
+
+    <div class="terms">
+      <input type="checkbox" id="terms" v-model="terms" required />
+      <label for="terms">Accept terms and conditions</label>
+    </div>
+
+    <button class="submit" type="submit">Create an Account</button>
   </form>
 </template>
 
@@ -20,7 +37,29 @@
         email: 'developer@mail.co.uk',
         password: '',
         role: 'developer',
+        terms: false,
+        tempSkill: '',
+        skills: JSON.parse(localStorage.getItem('skills')) || [],
       };
+    },
+    methods: {
+      addSkill(e) {
+        if (e.key === ',' && this.tempSkill) {
+          const skill = this.tempSkill.slice(0, this.tempSkill.length - 1);
+          if (!this.skills.includes(skill)) {
+            this.skills.push(skill);
+            localStorage.setItem('skills', JSON.stringify(this.skills));
+          }
+          this.tempSkill = '';
+        }
+      },
+      deleteSkill(skill) {
+        this.skills = this.skills.filter((item) => item !== skill);
+        localStorage.setItem('skills', JSON.stringify(this.skills));
+      },
+      onSubmit() {
+        console.log('submit!');
+      },
     },
   };
 </script>
@@ -43,7 +82,6 @@
     font-size: 0.875rem;
     font-weight: bold;
     letter-spacing: 1px;
-    /* padding-left: 0.5rem; */
     margin-bottom: 0.5rem;
   }
 
@@ -56,5 +94,116 @@
     color: #666;
     font-size: 1rem;
     margin-bottom: 3rem;
+    transition: border 0.3s ease-in;
+  }
+
+  input:focus {
+    border-color: rgb(248, 40, 109);
+  }
+
+  select {
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    -o-appearance: none;
+    background: url('../images/arrow-down.svg') no-repeat 98% center;
+    background-size: 0.8rem 0.8rem;
+  }
+
+  select:hover {
+    background: url('../images/arrow-up.svg') no-repeat 98% center;
+    background-size: 0.8rem 0.8rem;
+  }
+
+  input[type='checkbox'] {
+    appearance: none;
+    border: 2px solid #999;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 0.25rem;
+    display: inline-block;
+    margin: 0;
+    margin-right: 1rem;
+    margin-top: -2px;
+    cursor: pointer;
+    transition: all 0.3s ease-in;
+  }
+
+  input[type='checkbox']:hover {
+    background-color: #ddd;
+  }
+
+  #terms:checked {
+    background-image: url('../images/check.svg');
+    background-size: 0.8rem 0.8rem;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-color: rgb(248, 40, 109);
+    box-shadow: 0 0 1rem rgba(255, 22, 100, 0.5);
+    border-color: rgb(248, 40, 109);
+    /* transition: all 0.3s ease-in; */
+  }
+
+  #terms:checked:hover {
+    background-color: rgb(218, 35, 96);
+    border-color: rgb(218, 35, 96);
+    box-shadow: 0 0 0.25rem rgba(255, 22, 100, 0.5);
+    transform: scale(0.99);
+  }
+
+  .terms {
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    margin-top: 3rem;
+  }
+
+  .terms label {
+    display: inline-block;
+    margin: 0;
+  }
+
+  .skills {
+    text-transform: lowercase;
+    font-weight: bold;
+    /* color: rgb(248, 40, 109); */
+    color: #999;
+  }
+
+  .skills span {
+    display: inline-block;
+    letter-spacing: 1px;
+    background-color: #eee;
+    padding: 0.5rem 1rem;
+    border-radius: 3rem;
+    margin-right: 0.5rem;
+    margin-bottom: 1rem;
+    cursor: pointer;
+    transition: all 0.3s ease-out;
+  }
+
+  .skills span:hover {
+    background-color: #ddd;
+  }
+  .submit {
+    display: block;
+    margin: 3rem auto 0 auto;
+    padding: 1rem 1.75rem;
+    border: none;
+    border-radius: 6rem;
+    background-color: rgb(248, 40, 109);
+    box-shadow: 0 0 1.25rem rgba(255, 22, 100, 0.5);
+    color: #fff;
+    font-weight: bold;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    cursor: pointer;
+    transition: all 0.3s ease-in;
+  }
+
+  .submit:hover {
+    background-color: rgb(218, 35, 96);
+    box-shadow: 0 0 0.25rem rgba(255, 22, 100, 0.5);
+    transform: scale(0.99);
   }
 </style>
