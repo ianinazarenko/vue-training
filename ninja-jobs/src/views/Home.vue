11f0cd25473cd8b8180869471bbc31c378d148fb
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-04-19 22:16:17 +0300

    Added Jobs page

--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur
+      doloremque, ducimus quis sint molestias nulla adipisci voluptas cumque id
+      tempora nesciunt deleniti doloribus ea exercitationem libero unde dolore
+      rem voluptatibus recusandae dicta culpa odit assumenda nihil enim. Eum in
+      eius aut obcaecati, sint tenetur quibusdam eveniet optio ea eaque, impedit
+      temporibus voluptas. Possimus sit, tempora voluptatibus maxime nostrum
+      voluptates illo quo laboriosam! Enim eos voluptates itaque soluta quo,
+      magnam ut neque eius eligendi minus nulla ea necessitatibus provident
+      deleniti dicta facere quaerat sequi aspernatur exercitationem unde et
+      doloribus placeat laboriosam beatae. Id nulla aspernatur enim aut iste ea
+      itaque quia!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum aut aperiam
+      distinctio excepturi modi minus eius optio omnis et fugit, repellat
+      reiciendis iure aspernatur nisi laborum rerum a magnam cumque atque
+      adipisci nemo asperiores eveniet dicta inventore! Necessitatibus earum
+      aliquid officiis voluptatum. Sit sed atque neque nihil aut qui natus nobis
+      ratione reprehenderit temporibus eveniet autem impedit, voluptatum, amet
+      iste voluptates ex ad magni iure fuga quidem rerum! Incidunt enim a
+      tenetur dignissimos earum quasi reiciendis harum ut.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'Home',
+    components: {},
+  };
 </script>
---
+++ ninja-jobs/src/views/Jobs.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="jobs">
+    <h4 v-for="job in jobs" :key="job.id">
+      {{ job.title }}
+    </h4>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        jobs: [
+          {
+            title: 'UX Designer',
+            id: 1,
+            details: 'lorem',
+          },
+          {
+            title: 'Web Developer',
+            id: 2,
+            details: 'lorem',
+          },
+          {
+            title: 'Vue Developer ',
+            id: 3,
+            details: 'lorem',
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style></style>
