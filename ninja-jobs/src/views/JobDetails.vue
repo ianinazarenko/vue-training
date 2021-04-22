be6481d1a6fda9fed5e99c30ec612eef93573518
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-04-22 16:37:50 +0300

    Ninja-jobs is finished

--- ninja-jobs/src/views/JobDetails.vue
+++ ninja-jobs/src/views/JobDetails.vue
@@ -1,17 +1,30 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>Job id is: {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <h1>Loading job details...</h1>
+  </div>
 </template>
 
 <script>
   export default {
     props: ['id'],
-    // data() {
-    //   return {
-    //     id: this.$route.params.id,
-    //   };
-    // },
-  };
+    data() {
+      return {
+        job: null,
+      }
+    },
+    mounted() {
+      fetch('http://localhost:3000/jobs/' + this.id)
+        .then((response) => response.json())
+        .then((data) => {
+          setTimeout(() => (this.job = data), 1500)
+        })
+        .catch((err) => console.log(err))
+    },
+  }
 </script>
 
 <style></style>
--- ninja-jobs/src/views/Jobs.vue
+++ ninja-jobs/src/views/Jobs.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="jobs">
-    <h4 v-for="job in jobs" :key="job.id">
+    <h4 v-if="jobs.length" v-for="job in jobs" :key="job.id">
       <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
         {{ job.title }}
       </router-link>
     </h4>
+    <h4 v-else>Loading...</h4>
   </div>
 </template>
 
@@ -12,26 +13,16 @@
   export default {
     data() {
       return {
-        jobs: [
-          {
-            title: 'UX Designer',
-            id: 'ux-designer',
-            details: 'lorem',
-          },
-          {
-            title: 'Web Developer',
-            id: 'web-developer',
-            details: 'lorem',
-          },
-          {
-            title: 'Vue Developer ',
-            id: 'vue-developer',
-            details: 'lorem',
-          },
-        ],
-      };
+        jobs: [],
+      }
     },
-  };
+    mounted() {
+      fetch('http://localhost:3000/jobs')
+        .then((response) => response.json())
+        .then((data) => setTimeout(() => (this.jobs = data), 1000))
+        .catch((err) => console.log(err))
+    },
+  }
 </script>
 
 <style></style>
