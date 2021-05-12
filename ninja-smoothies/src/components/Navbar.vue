dfa5fb4a00274c79e870901a195214a193280e45
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-05-12 16:19:19 +0300

    Initial setup for ninja-smoothies project

---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>Navbar</nav>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+  }
+</script>
+
+<style></style>
---
+++ ninja-smoothies/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Home component</h1>
+</template>
+
+<script>
+  import Navbar from '../components/Navbar'
+
+  export default {
+    components: Navbar,
+  }
+</script>
+
+<style></style>
