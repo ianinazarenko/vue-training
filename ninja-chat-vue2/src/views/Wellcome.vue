7cfbe709091b2b4a4b13ef2416a581015d5d899a
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-05-18 19:04:54 +0300

    Create Wellcome page

---
+++ ninja-chat-vue2/src/views/Wellcome.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="wellcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Wellcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" id="name" v-model="name" />
+          <button type="submit" class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+
+  export default {
+    name: 'Wellcome',
+    components: {},
+    data() {
+      return {
+        name: null,
+      }
+    },
+    methods: {
+      enterChat() {
+        console.log(this.name)
+      },
+    },
+  }
+</script>
+
+<style>
+  .wellcome {
+    max-width: 400px;
+    margin-top: 100px;
+  }
+
+  .wellcome h2 {
+    font-size: 3em;
+  }
+
+  .wellcome button {
+    margin: 30px auto;
+  }
+</style>
--- ninja-smoothies/src/database/init.js
+++ ninja-smoothies/src/database/init.js
@@ -1,17 +1,4 @@
 import firebase from 'firebase'
-import firestore from 'firebase/firestore'
-
-// // Your web app's Firebase configuration
-// var firebaseConfig = {
-//   apiKey: 'AIzaSyAC6h5-FJr64kKbLvqj6P5yUoCzQhaSJRA',
-//   authDomain: 'udemy-ninja-smoothies-858e2.firebaseapp.com',
-//   projectId: 'udemy-ninja-smoothies-858e2',
-//   storageBucket: 'udemy-ninja-smoothies-858e2.appspot.com',
-//   messagingSenderId: '519119078754',
-//   appId: '1:519119078754:web:69f442b5ae1f6f85e4583b',
-// }
-// // Initialize Firebase
-// export default firebase.initializeApp(firebaseConfig)
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAC6h5-FJr64kKbLvqj6P5yUoCzQhaSJRA',
