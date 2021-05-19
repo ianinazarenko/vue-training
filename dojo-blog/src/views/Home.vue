2578660ed6a3305608bc56aeea6c03472a8af160
ynzrnk <71092723+ynzrnk@users.noreply.github.com>
2021-05-19 21:14:21 +0300

    Started composition API

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,30 +1,30 @@
 <template>
-  <section class="home">
-    <h1 ref="p">Hello, this is {{ name }} and I'm {{ age }}</h1>
-    <button @click="handleClick">Click me</button>
-  </section>
+  <div class="home">
+    <h1 ref="h">Hello, this is {{ name }} and I'm a {{ job }}</h1>
+    <button @click="handleClick">click me</button>
+  </div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import { ref } from 'vue'
+// @ is an alias to /src
+import { ref } from 'vue'
 
-  export default {
-    name: 'Home',
-    setup() {
-      const name = 'mario'
-      const age = 30
+export default {
+  name: 'Home',
+  components: {},
+  setup() {
+    const name = ref('Nino')
+    const job = ref('designer')
 
-      const p = ref('ho-ho-ho')
-      console.log(p)
-      console.log(p.value)
+    function handleClick() {
+      console.log("you've clicked me")
+      console.log(h.value)
+      name.value = 'mario'
+    }
 
-      function handleClick() {
-        console.log(p.value.textContent)
-        p.value.textContent = 'Hi'
-      }
+    const h = ref(null)
 
-      return { name, age, handleClick, p }
-    },
-  }
+    return { name: name, job, handleClick, h }
+  },
+}
 </script>
